207bffdc950109886c992deab20aa37ecb36dfcf
章芝源 <610925874@qq.com>
2016-01-01 13:04:02 +0800

    drawRect被调用时机

---
+++ 01基本练习/01基本练习/testView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  01基本练习
+//
+//  Created by 章芝源 on 16/1/1.
+//  Copyright © 2016年 ZZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testView : UIView
+
+@end
---
+++ 01基本练习/01基本练习/testView.m
@@ -0,0 +1,17 @@
+//
+//  testView.m
+//  01基本练习
+//
+//  Created by 章芝源 on 16/1/1.
+//  Copyright © 2016年 ZZY. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+//
+- (void)drawRect:(CGRect)rect
+{
+    NSLog(@"%s", __func__);
+}
+@end
