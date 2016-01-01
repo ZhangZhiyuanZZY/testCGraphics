26df0a7724e3b5550474d24733473faea61ab3dd
章芝源 <610925874@qq.com>
2016-01-01 15:34:35 +0800

    画两条线的问题

--- 01基本练习/01基本练习/testView.m
+++ 01基本练习/01基本练习/testView.m
@@ -42,7 +42,65 @@
 //    [self drawLine];
     //贝塞尔
 //    [self drawLine2];
-    [self drawLine3];
+    [self drawLine7];
+}
+- (void)drawLine7
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    UIBezierPath *bzrPath = [UIBezierPath bezierPath];
+    [bzrPath moveToPoint:CGPointMake(30, 30)];
+    [bzrPath addLineToPoint:CGPointMake(300, 300)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+
+    //面向对象思想, 当上一个bzrPath画好已经添加到了图形上下文, 这个路径就没什么用了,  我们可以给这个路径重新画线,然后再次添加到图形上下文
+    UIBezierPath *bzrPath1 = [UIBezierPath bezierPath];//貌似一个图形上下文中. 路径只能存在一条?
+    [bzrPath moveToPoint:CGPointMake(100, 100)];
+    [bzrPath addLineToPoint:CGPointMake(400, 100)];
+    CGContextAddPath(ctx, bzrPath1.CGPath);
+    CGContextStrokePath(ctx);
+}
+
+//不相连的线使用不同路径
+- (void)drawLine6
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    UIBezierPath *bzrPath = [UIBezierPath bezierPath];
+    [bzrPath moveToPoint:CGPointMake(30, 30)];
+    [bzrPath addLineToPoint:CGPointMake(300, 300)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+    
+    //面向对象思想, 当上一个bzrPath画好已经添加到了图形上下文, 这个路径就没什么用了,  我们可以给这个路径重新画线,然后再次添加到图形上下文
+    bzrPath = [UIBezierPath bezierPath];
+    [bzrPath moveToPoint:CGPointMake(50, 100)];
+    [bzrPath addLineToPoint:CGPointMake(400, 100)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+    CGContextStrokePath(ctx);
+}
+
+//不相连的线, 使用同一个路径
+- (void)drawLine5
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    UIBezierPath *bzrPath = [UIBezierPath bezierPath];
+    [bzrPath moveToPoint:CGPointMake(30, 30)];
+    [bzrPath addLineToPoint:CGPointMake(300, 300)];
+
+    [bzrPath moveToPoint:CGPointMake(50, 100)];
+    [bzrPath addLineToPoint:CGPointMake(400, 100)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+    CGContextStrokePath(ctx);
+}
+
+//画两条线  连线
+- (void)drawLine4
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    UIBezierPath *bzrPath = [UIBezierPath bezierPath];
+    [bzrPath moveToPoint:CGPointMake(30, 30)];
+    [bzrPath addLineToPoint:CGPointMake(300, 300)];
+    [bzrPath addLineToPoint:CGPointMake(200, 400)];
+    CGContextAddPath(ctx, bzrPath.CGPath);
+    [bzrPath stroke];
 }
 //最简洁写法
 - (void)drawLine3
